b98dbf0bb689b1209a00f234c1ed8f66c43b71c2
Eduardo Andreoli <53129882+C3Andreoli@users.noreply.github.com>
2025-06-22 19:34:29 -0300

    Background Modeling Function
    New version of background modeling tool. Using OpenCV2 MOG2 background remover. 
    
    The code will auto recognize if CUDA is available.

---
+++ VideoPreprocessor/backgroundRemover.py
@@ -0,0 +1,133 @@
+import cv2
+from cv2 import cuda
+
+
+class BGRemover:
+
+    def __init__(self, videoPath,outputRed,outputBG, GUI = False):
+        self.capture = cv2.VideoCapture(videoPath)
+        self.total_frames = int(self.capture.get(cv2.CAP_PROP_FRAME_COUNT))  
+        self.progress_step = max(int(self.total_frames * 0.05), 1)  
+        self.last_progress = -1  
+        self.GUI = GUI
+        if self.GUI:
+            cv2.namedWindow('Original', cv2.WINDOW_NORMAL)
+            cv2.namedWindow('BG ESTATICO', cv2.WINDOW_NORMAL)
+            cv2.namedWindow('OBJECTS IN RED', cv2.WINDOW_NORMAL)
+
+
+        self.startOutput(outputRed,outputBG)
+        
+        self.checkIfCUDADevice()
+        self.checkCapture()
+
+    def checkIfCUDADevice(self):
+        self.CUDA_enable = cuda.getCudaEnabledDeviceCount() > 0
+
+    def checkCapture(self):
+        if not self.capture.isOpened():
+            raise RuntimeError("O vídeo não foi carregado!!!!!")
+        else:
+            print("Vídeo foi carregado com sucesso")
+
+    def startOutput(self,outputRed,outputBG):
+        self.width = int(self.capture.get(cv2.CAP_PROP_FRAME_WIDTH))
+        self.height = int(self.capture.get(cv2.CAP_PROP_FRAME_HEIGHT))
+        self.fps = self.capture.get(cv2.CAP_PROP_FPS) or 30.0  # fallback
+
+        self.writerRed = cv2.VideoWriter(outputRed,cv2.VideoWriter_fourcc(*'mp4v'), self.fps, (self.width, self.height))
+        self.writerBG = cv2.VideoWriter(outputBG, cv2.VideoWriter_fourcc(*'mp4v'), self.fps, (self.width, self.height))
+
+    def startBGSubtract_CUDA(self):
+        self.bgSubtractor = cuda.createBackgroundSubtractorMOG2()
+        self.stream = cuda.Stream()
+        current_frame = 0
+        while True:
+            
+            ret, frame = self.capture.read()
+            if not ret:
+                break
+
+            current_frame += 1
+            progress = int((current_frame / self.total_frames) * 100)
+            if progress % 5 == 0 and progress != self.last_progress:
+                print(f"Progresso: {progress}%")
+                self.last_progress = progress
+                
+            gpu_frame = cv2.cuda_GpuMat()
+            gpu_frame.upload(frame, self.stream)
+
+            gpu_fg_mask = self.bgSubtractor.apply(gpu_frame, learningRate=0, stream=self.stream)
+            gpuBackground = self.bgSubtractor.getBackgroundImage(stream=self.stream)
+
+            self.stream.waitForCompletion()
+
+            fg_mask = gpu_fg_mask.download()
+            if not gpuBackground.empty():
+                background = gpuBackground.download()
+                highlighted_view = background.copy()
+                highlighted_view[fg_mask == 255] = (0, 0, 255)
+                if self.GUI:
+                    cv2.imshow('Original', frame)
+                    cv2.imshow('BG ESTATICO', background)
+                    cv2.imshow('OBJECTS IN RED', highlighted_view)
+
+                self.writerRed.write(highlighted_view)
+                self.writerBG.write(background)
+
+            if self.GUI:
+                if cv2.waitKey(25) & 0xFF == ord('q'):
+                    break
+
+    def startBGSubtract(self):
+        self.bgSubtractor = cv2.createBackgroundSubtractorMOG2()
+        current_frame = 0
+        while True:
+            
+            ret, frame = self.capture.read()
+            if not ret:
+                break
+
+            current_frame += 1
+            progress = int((current_frame / self.total_frames) * 100)
+            if progress % 5 == 0 and progress != self.last_progress:
+                print(f"Progresso: {progress}%")
+                self.last_progress = progress
+
+            fg_mask = self.bgSubtractor.apply(frame)
+
+            background = self.bgSubtractor.getBackgroundImage()
+            if background is not None:
+                highlighted_view = background.copy()
+                highlighted_view[fg_mask == 255] = (0, 0, 255)
+                if self.GUI:
+                    cv2.imshow('Original', frame)
+                    cv2.imshow('BG ESTATICO', background)
+                    cv2.imshow('OBJECTS IN RED', highlighted_view)
+
+                self.writerRed.write(highlighted_view)
+                self.writerBG.write(background)
+                if self.GUI:
+                    if cv2.waitKey(25) & 0xFF == ord('q'):
+                        break
+
+    def BGSubtractor(self):
+        if self.CUDA_enable:
+            print("CUDA habilitado: usando GPU")
+            self.startBGSubtract_CUDA()
+        else:
+            print("CUDA não disponível: usando CPU")
+            self.startBGSubtract()
+
+        self.capture.release()
+        self.writerRed.release()
+        self.writerBG.release()
+        cv2.destroyAllWindows()
+
+if __name__ == '__main__':
+    
+    path = 'Train_data/aic21-track4-train-data/aic21-track4-train-data/2.mp4'
+    
+    subtractor = BGRemover(path,'red.mp4', 'BG.mp4',GUI=True)
+    subtractor.CUDA_enable = False
+    subtractor.BGSubtractor()
